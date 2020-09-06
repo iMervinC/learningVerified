1a33aee45f48e439f011bf1f5b19ffa844f20353
Mervin Christian T. Itorma <40752994+iMervinC@users.noreply.github.com>
2020-09-06 23:02:32 +0800

    Updates

---
+++ precache-manifest.c3d4026a33eab9f79e7f9340762d3046.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b464a21cc0e21c545e37204971188d9",
+    "url": "/learningVerified/index.html"
+  },
+  {
+    "revision": "ff7832a3f47930250b83",
+    "url": "/learningVerified/static/css/main.2b1ada8d.chunk.css"
+  },
+  {
+    "revision": "ee547600f250303983b1",
+    "url": "/learningVerified/static/js/2.16e8fa74.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/learningVerified/static/js/2.16e8fa74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff7832a3f47930250b83",
+    "url": "/learningVerified/static/js/main.8e6eec23.chunk.js"
+  },
+  {
+    "revision": "f04a76e260e79e4f2927",
+    "url": "/learningVerified/static/js/runtime-main.41b93f6a.js"
+  },
+  {
+    "revision": "ff30649b40f369e508e9dc551f5e0e6f",
+    "url": "/learningVerified/static/media/logo.ff30649b.png"
+  }
+]);
