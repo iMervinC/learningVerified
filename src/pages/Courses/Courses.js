0a3a55738580ee00f1df2df8af6b11b36be613d9
Mervin Christian T. Itorma <40752994+iMervinC@users.noreply.github.com>
2020-09-06 22:58:09 +0800

    GH Pages

--- src/pages/Courses/Courses.js
+++ src/pages/Courses/Courses.js
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Card } from "../../components/Card/Card";
 import { CoursesData } from "../../data/CoursesData";
 import { SmallBanner } from "../../components/Banner";
+import { Context } from "../../context/CartContext";
 
 const Courses = () => {
+  const { addToCart } = useContext(Context);
+
   return (
     <div>
       <SmallBanner title={"Our Online MCLE Courses"} />
       <div className="container">
         <h3 className="label ">Courses</h3>
         <div className="row">
-          {CoursesData.map((item, index) => {
+          {CoursesData.map((course, index) => {
             return (
               <Card
                 key={index}
-                body={item.title}
-                footer={item.by}
-                imgLink={item.imgUrl}
+                body={course.title}
+                footer={course.by}
+                imgLink={course.imgUrl}
+                onClick={() => addToCart(course)}
               />
             );
           })}
